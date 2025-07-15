8c669931934eb47f8a4c1aa6904cf57cc4c75b5a
Frank Denis <github@pureftpd.org>
2025-07-15 15:41:23 +0200

    CLI: support a default key for the MAC operation

--- hiae-cli/src/hiae.c
+++ hiae-cli/src/hiae.c
@@ -219,8 +219,13 @@ load_key_material(cli_options_t *opts, hiae_key_material_t *km)
             return -1;
         }
     } else {
-        fprintf(stderr, "Error: No key provided (use -k or -kf)\n");
-        return -1;
+        // Use default key if none provided
+        uint8_t default_key[HIAE_KEY_SIZE] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+                                               0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+                                               0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+                                               0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f };
+        memcpy(km->key, default_key, HIAE_KEY_SIZE);
+        km->key_loaded = 1;
     }
 
     // Load or generate nonce
