27c4f285f1a7fdbdfbb33ccda5d7fda0478c196a
Frank Denis <github@pureftpd.org>
2026-05-27 14:15:24 +0200

    Update for zig-current

--- build.zig
+++ build.zig
@@ -5,26 +5,26 @@ pub fn build(b: *std.Build) void {
     const optimize = .ReleaseFast;
     const version = std.SemanticVersion.parse("0.2.2") catch unreachable;
 
+    const lib_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .strip = true,
+        .link_libc = true,
+    });
+
     const lib = b.addLibrary(.{
         .name = "hiae",
         .version = version,
         .linkage = .static,
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .strip = true,
-        }),
+        .root_module = lib_mod,
     });
 
-    lib.linkLibC();
-
-    lib.addIncludePath(b.path("include"));
+    lib_mod.addIncludePath(b.path("include"));
 
     const source_files = &.{
         "src/hiae/HiAE_aesni.c",
         "src/hiae/HiAE_arm.c",
         "src/hiae/HiAE_arm_sha3.c",
-        "src/hiae/HiAE_arm.c",
         "src/hiae/HiAE_software.c",
         "src/hiae/HiAE_stream.c",
         "src/hiae/HiAE_vaes_avx512.c",
@@ -46,11 +46,8 @@ pub fn build(b: *std.Build) void {
         "src/hiaex4/HiAEx4.c",
     };
 
-    lib.addCSourceFiles(.{ .files = source_files });
+    lib_mod.addCSourceFiles(.{ .files = source_files });
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(lib);
 
     b.installDirectory(.{
@@ -59,143 +56,39 @@ pub fn build(b: *std.Build) void {
         .source_dir = b.path("include"),
     });
 
-    const function_test_source_files = &.{
-        "test/function_test.c",
+    const TestSpec = struct { name: []const u8, src: []const u8, run: bool };
+    const test_specs = [_]TestSpec{
+        .{ .name = "function_test", .src = "test/function_test.c", .run = true },
+        .{ .name = "test_vectors_ietf", .src = "test/test_vectors_ietf.c", .run = true },
+        .{ .name = "test_vectors_hiaex2", .src = "test/test_vectors_hiaex2.c", .run = true },
+        .{ .name = "test_stream", .src = "test/test_stream.c", .run = true },
+        .{ .name = "perf_test", .src = "test/performance_test.c", .run = false },
+        .{ .name = "perf_x2_test", .src = "test/performance_x2_test.c", .run = false },
+        .{ .name = "perf_x4_test", .src = "test/performance_x4_test.c", .run = false },
     };
-    const functions_test = b.addExecutable(.{
-        .name = "function_test",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .strip = false,
-        }),
-    });
-    functions_test.addCSourceFiles(.{ .files = function_test_source_files });
-    functions_test.linkLibC();
-    functions_test.addIncludePath(b.path("include"));
-    functions_test.linkLibrary(lib);
 
-    const test_vectors_test_source_files = &.{
-        "test/test_vectors_ietf.c",
-    };
-    const test_vectors_test = b.addExecutable(.{
-        .name = "test_vectors_ietf",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .strip = false,
-        }),
-    });
-    test_vectors_test.addCSourceFiles(.{ .files = test_vectors_test_source_files });
-    test_vectors_test.linkLibC();
-    test_vectors_test.addIncludePath(b.path("include"));
-    test_vectors_test.linkLibrary(lib);
-
-    const test_vectors_hiaex2_test_source_files = &.{
-        "test/test_vectors_hiaex2.c",
-    };
-    const test_vectors_hiaex2_test = b.addExecutable(.{
-        .name = "test_vectors_hiaex2",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .strip = false,
-        }),
-    });
-    test_vectors_hiaex2_test.addCSourceFiles(.{ .files = test_vectors_hiaex2_test_source_files });
-    test_vectors_hiaex2_test.linkLibC();
-    test_vectors_hiaex2_test.addIncludePath(b.path("include"));
-    test_vectors_hiaex2_test.linkLibrary(lib);
-
-    const stream_test_source_files = &.{
-        "test/test_stream.c",
-    };
-    const stream_test = b.addExecutable(.{
-        .name = "test_stream",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .strip = false,
-        }),
-    });
-    stream_test.addCSourceFiles(.{ .files = stream_test_source_files });
-    stream_test.linkLibC();
-    stream_test.addIncludePath(b.path("include"));
-    stream_test.linkLibrary(lib);
-
-    const performance_test_source_files = &.{
-        "test/performance_test.c",
-    };
-    const performance_test = b.addExecutable(.{
-        .name = "perf_test",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .strip = false,
-        }),
-    });
-    performance_test.addCSourceFiles(.{ .files = performance_test_source_files });
-    performance_test.linkLibC();
-    performance_test.addIncludePath(b.path("include"));
-    performance_test.linkLibrary(lib);
-
-    b.installArtifact(functions_test);
-    b.installArtifact(test_vectors_test);
-    b.installArtifact(test_vectors_hiaex2_test);
-    b.installArtifact(stream_test);
-    b.installArtifact(performance_test);
-
-    const performance_x2_test_source_files = &.{
-        "test/performance_x2_test.c",
-    };
-    const performance_x2_test = b.addExecutable(.{
-        .name = "perf_x2_test",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .strip = false,
-        }),
-    });
-    performance_x2_test.addCSourceFiles(.{ .files = performance_x2_test_source_files });
-    performance_x2_test.linkLibC();
-    performance_x2_test.addIncludePath(b.path("include"));
-    performance_x2_test.linkLibrary(lib);
-
-    const performance_x4_test_source_files = &.{
-        "test/performance_x4_test.c",
-    };
-    const performance_x4_test = b.addExecutable(.{
-        .name = "perf_x4_test",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .strip = false,
-        }),
-    });
-    performance_x4_test.addCSourceFiles(.{ .files = performance_x4_test_source_files });
-    performance_x4_test.linkLibC();
-    performance_x4_test.addIncludePath(b.path("include"));
-    performance_x4_test.linkLibrary(lib);
-
-    b.installArtifact(functions_test);
-    b.installArtifact(test_vectors_test);
-    b.installArtifact(stream_test);
-    b.installArtifact(performance_test);
-    b.installArtifact(performance_x2_test);
-    b.installArtifact(performance_x4_test);
-
-    // Create a test step
     const test_step = b.step("test", "Run all tests");
 
-    // Add run steps for each test
-    const run_functions_test = b.addRunArtifact(functions_test);
-    const run_test_vectors_test = b.addRunArtifact(test_vectors_test);
-    const run_test_vectors_hiaex2_test = b.addRunArtifact(test_vectors_hiaex2_test);
-    const run_stream_test = b.addRunArtifact(stream_test);
+    for (test_specs) |spec| {
+        const exe_mod = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+            .strip = false,
+            .link_libc = true,
+        });
+        exe_mod.addCSourceFile(.{ .file = b.path(spec.src), .flags = &.{} });
+        exe_mod.addIncludePath(b.path("include"));
+        exe_mod.linkLibrary(lib);
 
-    // Add them as dependencies of the test step
-    test_step.dependOn(&run_functions_test.step);
-    test_step.dependOn(&run_test_vectors_test.step);
-    test_step.dependOn(&run_test_vectors_hiaex2_test.step);
-    test_step.dependOn(&run_stream_test.step);
+        const exe = b.addExecutable(.{
+            .name = spec.name,
+            .root_module = exe_mod,
+        });
+        b.installArtifact(exe);
+
+        if (spec.run) {
+            const run = b.addRunArtifact(exe);
+            test_step.dependOn(&run.step);
+        }
+    }
 }
